f9ca6e41a2ec279359771ba06ca4c8093201e94c
axelperezg <133084869+axelperezg@users.noreply.github.com>
2025-09-17 23:52:30 +0000

    CREATED estructura base

---
+++ src/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+
+/*
+* Rutas para el recurso Evento.
+*/
+
+// Recuperar todos los eventos
+Route::get('/eventos', [EventoController::class, 'index']);
+// Almacenar un evento nuevo
+Route::post('/eventos', [EventoController::class, 'store']);
+// Recuperar un evento específico
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+// Actualizar un evento específico
+Route::put('/eventos/{evento}', [EventoController::class, 'update']);
+// Eliminar un evento específico
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
